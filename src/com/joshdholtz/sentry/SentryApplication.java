f1cd2be6ff7a1c3ec6c383ae2a55792cf3354d1e
Josh Holtz <josh@rokkincat.com>
2014-01-02 09:55:48 -0600

    Committing stuff so I can merge a PR

--- src/com/joshdholtz/sentry/SentryApplication.java
+++ src/com/joshdholtz/sentry/SentryApplication.java
@@ -1,5 +1,8 @@
 package com.joshdholtz.sentry;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.JSONException;
 
 import com.joshdholtz.sentry.Sentry.SentryEventBuilder;
@@ -29,6 +32,7 @@ public class SentryApplication extends Application {
 				// Sets extra key if wifi is connected
 				try {
 					builder.getExtra().put("wifi", String.valueOf(mWifi.isConnected()));
+					builder.getTags().put("tag_1", "hello");
 				} catch (JSONException e) {}
 
 				return builder;
@@ -37,15 +41,20 @@ public class SentryApplication extends Application {
 		});
 		
 		// Sentry will look for uncaught exceptions from previous runs and send them        
-		Sentry.init(this, "YOUR-DSN");
+		Sentry.init(this, "https://app.getsentry.com", "YOUR-DSN");
 
 		
-		// Capture event
-		Sentry.captureEvent(new Sentry.SentryEventBuilder()
-			.setMessage("Being so awesome at stuff 2")
-			.setCulprit("Josh D Holtz")
-			.setTimestamp(System.currentTimeMillis())
-		);
+//		Map<String, String> tags = new HashMap<String, String>();
+//		tags.put("tag1", "value1");
+//		tags.put("tag2", "value2");
+//		
+//		// Capture event
+//		Sentry.captureEvent(new Sentry.SentryEventBuilder()
+//			.setMessage("Being so awesome at stuff 3")
+//			.setCulprit("Josh D Holtz")
+//			.setTags(tags)
+//			.setTimestamp(System.currentTimeMillis())
+//		);
 		
 	}
 	
