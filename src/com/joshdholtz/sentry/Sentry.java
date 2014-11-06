c496d821534729443aa4ac7d9ffab32cec6c2edc
Krasimir Nikolov <krasimir.nikolov@mtr-design.com>
2014-11-06 11:00:30 +0200

    Fixed a problem with access to SSL.

--- src/com/joshdholtz/sentry/Sentry.java
+++ src/com/joshdholtz/sentry/Sentry.java
@@ -16,11 +16,20 @@ import java.io.StreamCorruptedException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,10 +39,18 @@ import java.util.Map;
 import java.util.TimeZone;
 import java.util.UUID;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
@@ -280,7 +297,75 @@ public class Sentry {
 	    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 	    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
 	}
-	
+
+	public static class ExSSLSocketFactory extends SSLSocketFactory {
+		  SSLContext sslContext = SSLContext.getInstance("TLS");
+
+		    public ExSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+		        super(truststore);
+		        TrustManager x509TrustManager = new X509TrustManager() {
+		            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		            }
+
+		            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		            }
+
+		            public X509Certificate[] getAcceptedIssuers() {
+		                return null;
+		            }
+		        };
+
+		        sslContext.init(null, new TrustManager[] { x509TrustManager }, null);
+		    }
+
+		    public ExSSLSocketFactory(SSLContext context) throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+		       super(null);
+		       sslContext = context;
+		    }
+
+		    @Override
+		    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+		        return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+		    }
+
+		    @Override
+		    public Socket createSocket() throws IOException {
+		        return sslContext.getSocketFactory().createSocket();
+		    }
+		}
+
+	public static HttpClient getHttpsClient(HttpClient client) {
+	     try {
+			   X509TrustManager x509TrustManager = new X509TrustManager() {
+					@Override
+					public void checkClientTrusted(X509Certificate[] chain,
+							String authType) throws CertificateException {
+					}
+
+					@Override
+					public void checkServerTrusted(X509Certificate[] chain,
+							String authType) throws CertificateException {
+					}
+
+					@Override
+					public X509Certificate[] getAcceptedIssuers() {
+						return null;
+					}
+		        };
+
+		        SSLContext sslContext = SSLContext.getInstance("TLS");
+		        sslContext.init(null, new TrustManager[]{x509TrustManager}, null);
+		        SSLSocketFactory sslSocketFactory = new ExSSLSocketFactory(sslContext);
+		        sslSocketFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+		        ClientConnectionManager clientConnectionManager = client.getConnectionManager();
+		        SchemeRegistry schemeRegistry = clientConnectionManager.getSchemeRegistry();
+		        schemeRegistry.register(new Scheme("https", sslSocketFactory, 443));
+		        return new DefaultHttpClient(clientConnectionManager, client.getParams());
+		    } catch (Exception ex) {
+		        return null;
+		    }
+		}
+
 	private static void doCaptureEventPost(final SentryEventRequest request) {
 		
 		if (!shouldAttemptPost()) {
@@ -292,7 +377,7 @@ public class Sentry {
 			@Override
 			protected Void doInBackground(Void... params) {
 				
-				HttpClient httpClient = new DefaultHttpClient();
+				HttpClient httpClient = getHttpsClient(new DefaultHttpClient());
 				HttpPost httpPost = new HttpPost(Sentry.getInstance().baseUrl + "/api/" + getProjectId() + "/store/");
 
 				int TIMEOUT_MILLISEC = 10000;  // = 20 seconds
